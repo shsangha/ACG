cbe95adcd5ea0ed1f0986a3b69d8510eb2bf7842
shsangha <sssangha@ucalgary.ca>
2019-12-10 22:39:57 -0700

    added resuable about section for landing and about page with intersection observer triggered reveal animations

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Main from "../sections/index/slideshow"
+import Header from "../components/header"
+import About from "../sections/index/about"
+import Footer from "../components/footer"
+
+const AboutPage = () => (
+  <>
+    <Header />
+    <About />
+    <Footer />
+  </>
+)
+export default AboutPage
