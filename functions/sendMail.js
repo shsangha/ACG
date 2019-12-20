a646ace7449fe501a887fd01f00a3b966d889b1a
shsangha <sssangha@ucalgary.ca>
2019-12-19 20:56:04 -0700

    sends mail for contact/callback requests

---
+++ functions/sendMail.js
@@ -0,0 +1,91 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const mailgun = require("mailgun-js")
+
+const mg = mailgun({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN,
+})
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+}
+
+const handler = (event, _, callback) => {
+  const data = JSON.parse(event.body)
+
+  const { name, mail, phone, msg, method } = data
+
+  const mailOptions = {
+    from: `${name} <${mail}>`,
+    to: "shawnsangha9@gmail.com",
+    replyTo: mail,
+    subject: `Email from website contact form`,
+    text: `${msg || " "}`,
+  }
+
+  const phoneOptions = {
+    from: "cbrequest@albertacommercialgroup.com",
+    to: "shawnsangha9@gmail.com",
+    replyTo: "cbrequest@albertacommercialgroup.com",
+    subject: `Callback request from ${name} - ${phone}`,
+    text: `${msg || " "}`,
+  }
+
+  if (!name) {
+    callback(null, {
+      statusCode: 422,
+      headers,
+      body: JSON.stringify({ error: "Name is a required field" }),
+    })
+    return
+  }
+  if (method === "mail" && mail) {
+    mg.messages().send(mailOptions, (error, body) => {
+      if (error) {
+        callback(null, {
+          statusCode: 400,
+          headers,
+          body: JSON.stringify({
+            error: "Invalid email address check and try again",
+          }),
+        })
+      } else {
+        callback(null, {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify(body),
+        })
+      }
+    })
+  } else if (method === "phone" && phone) {
+    mg.messages().send(phoneOptions, (error, body) => {
+      if (error) {
+        callback(null, {
+          statusCode: 400,
+          headers,
+          body: JSON.stringify({
+            error: "Check form inputs and try again",
+          }),
+        })
+      } else {
+        callback(null, {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify(body),
+        })
+      }
+    })
+  } else {
+    callback(null, {
+      statusCode: 422,
+      headers,
+      body: JSON.stringify({ error: "Check form inputs and try again" }),
+    })
+  }
+}
+
+exports.handler = handler
