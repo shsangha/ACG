e8db8e41d57463515c924bf9a397d26835815e92
shsangha <sssangha@ucalgary.ca>
2019-12-19 20:56:55 -0700

    contact page

---
+++ src/pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+import Contact from "../sections/contact"
+
+const ContactPage = () => (
+  <>
+    <Header />
+    <Contact />
+  </>
+)
+
+export default ContactPage
