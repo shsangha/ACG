947c9a041825c6fe8b43cf52ed5383540c15d661
shsangha <sssangha@ucalgary.ca>
2019-12-19 20:58:24 -0700

    listings page

---
+++ src/pages/listings.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+import Listings from "../sections/listings"
+
+const ListingsPage = () => (
+  <>
+    <Header />
+    <Listings />
+  </>
+)
+
+export default ListingsPage
