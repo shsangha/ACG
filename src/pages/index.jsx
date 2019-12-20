3962450f17b88c8bb7f35bd9b60f1436be103687
shsangha <sssangha@ucalgary.ca>
2019-12-19 20:57:07 -0700

    index page

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Main from "../sections/index/slideshow"
+import Header from "../components/header"
+import About from "../sections/index/about"
+import Footer from "../components/footer"
+
+const Index = () => (
+  <>
+    <Header />
+    <Main />
+    <About />
+    <Footer />
+  </>
+)
+export default Index
