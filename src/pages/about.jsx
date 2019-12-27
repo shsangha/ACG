58656ab9e4c91b9e4d81adf2e8404b15c3a72b75
shsangha <sssangha@ucalgary.ca>
2019-12-27 16:09:59 -0700

    removed import of landing section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import Main from "../sections/index/slideshow"
 import Header from "../components/header"
 import About from "../sections/index/about"
 import Footer from "../components/footer"
