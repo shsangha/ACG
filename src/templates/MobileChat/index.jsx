89921b512260c13ffef24aee942797e0658ac290
shsangha <sssangha@ucalgary.ca>
2019-12-29 23:09:41 -0700

    contact form within the listing page

---
+++ src/templates/MobileChat/index.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react"
+import { Transition } from "react-transition-group"
+import { TimelineLite, Power1 } from "gsap"
+import ContactForm from "../../sections/contact/contactForm"
+import "./style.scss"
+
+const MobileChat = () => {
+  const [formOpen, setFormOpen] = useState(false)
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          new TimelineLite()
+            .to(".container", 0.5, {
+              scrollTop: 0,
+            })
+            .add(() => setFormOpen(true))
+        }}
+        className="listing_hero_chat_link black"
+      >
+        Contact an Agent
+      </button>
+      <Transition
+        unmountOnExit
+        in={formOpen}
+        timeout={1200}
+        onEntering={() => {
+          new TimelineLite()
+            .set(".container", { overflowY: "hidden" })
+            .to(".listing_hero_chat_container", 0.7, {
+              scaleY: 1,
+              ease: Power1.easeOut,
+            })
+            .to(".listing_hero_chat_wrapper", 0.5, {
+              opacity: 1,
+              ease: Power1.easeOut,
+            })
+        }}
+        onExiting={() => {
+          new TimelineLite()
+            .set(".container", { overflowY: "scroll" })
+
+            .to(".listing_hero_chat_wrapper", 0.5, {
+              opacity: 0,
+              ease: Power1.easeIn,
+            })
+            .to(".listing_hero_chat_container", 0.7, {
+              scaleY: 0,
+              ease: Power1.easeIn,
+            })
+        }}
+      >
+        <div className="listing_hero_chat_container">
+          <div className="listing_hero_chat_wrapper">
+            <div className="listing_hero_mobile_header">
+              <h2 className="listing_hero_mobile_header_text">Get In Touch</h2>
+              <button
+                onClick={() => {
+                  setFormOpen(false)
+                }}
+                className="listing_hero_mobile_close_btn"
+              >
+                X
+              </button>
+            </div>
+            <ContactForm
+              subject="listing page"
+              tabTrab={true}
+              callback={() => setFormOpen(false)}
+            />
+          </div>
+        </div>
+      </Transition>
+    </>
+  )
+}
+
+export default MobileChat
