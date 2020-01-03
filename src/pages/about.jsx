ea56feb6f842bb8c4b8b683647f014b67db507a2
shsangha <sssangha@ucalgary.ca>
2020-01-02 21:45:10 -0700

    passed through props to be able to have access to location

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,13 +1,5 @@
 import React from "react"
-import Header from "../components/header"
 import About from "../sections/index/about"
-import Footer from "../components/footer"
 
-const AboutPage = () => (
-  <>
-    <Header />
-    <About />
-    <Footer />
-  </>
-)
+const AboutPage = props => <About {...props} />
 export default AboutPage
