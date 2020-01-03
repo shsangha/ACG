e12636914a144df4f26f585babbc0431fa90012f
shsangha <sssangha@ucalgary.ca>
2020-01-02 21:45:56 -0700

    removed unsused imports

--- src/pages/listings.jsx
+++ src/pages/listings.jsx
@@ -1,12 +1,6 @@
 import React from "react"
-import Header from "../components/header"
 import Listings from "../sections/listings"
 
-const ListingsPage = () => (
-  <>
-    <Header />
-    <Listings />
-  </>
-)
+const ListingsPage = () => <Listings />
 
 export default ListingsPage
