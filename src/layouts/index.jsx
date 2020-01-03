5cfd9f892aa90da40baa841b1a2f2b7451b3df1e
shsangha <sssangha@ucalgary.ca>
2020-01-02 21:44:37 -0700

    added transition group wrapper around layout to animate page changes

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,10 +1,66 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "./style.scss"
+import { TransitionGroup, Transition } from "react-transition-group"
+import { TimelineLite, Power2 } from "gsap"
+import Seo from "../components/seo"
+import Header from "../components/header"
+import Footer from "../components/footer"
 
-const Layout = props => (
-  <div id="container" className="container">
-    {props.children}
-  </div>
-)
+let exitTimeline, enterTimeline
+
+const Layout = props => {
+  useEffect(() => {
+    new TimelineLite()
+      .to(".transition_slide", 1.5, {
+        x: "-100vw",
+        ease: Power2.easeIn,
+      })
+      .to(".container", 1.5, { opacity: 1, ease: Power2.easeIn }, "-=1.25")
+  }, [])
+
+  return (
+    <TransitionGroup>
+      <Transition
+        key={props.location.pathname}
+        timeout={{
+          enter: 2000,
+          exit: 600,
+        }}
+        appear={false}
+        onEntering={node => {
+          if (enterTimeline) {
+            enterTimeline.progress(100)
+            enterTimeline.clear()
+          }
+
+          enterTimeline = new TimelineLite()
+            .set(".transition_slide", { x: "100vw" })
+            .to(".transition_slide", 0.75, { x: 0 })
+            .to(".transition_slide", 0.75, { x: "-100vw" })
+            .to(node, 1, { opacity: 1, ease: Power2.easeIn }, "-=0.6")
+        }}
+        onExiting={node => {
+          if (exitTimeline) {
+            exitTimeline.progress(100)
+            exitTimeline.clear()
+          }
+
+          exitTimeline = new TimelineLite().to(node, 0.6, {
+            opacity: 0,
+            ease: Power2.easeOut,
+          })
+        }}
+      >
+        <div id="container" className="container">
+          <Seo />
+          <Header />
+          {props.children}
+          <Footer />
+          <div className="transition_slide" />
+        </div>
+      </Transition>
+    </TransitionGroup>
+  )
+}
 
 export default Layout
