ebb0d6aeed6b74cba4f9f80ab08d09876fbd5458
shsangha <sssangha@ucalgary.ca>
2020-01-02 21:45:41 -0700

    removed unsused imports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,11 @@
 import React from "react"
 import Main from "../sections/index/slideshow"
-import Header from "../components/header"
 import About from "../sections/index/about"
-import Footer from "../components/footer"
 
-const Index = () => (
+const Index = props => (
   <>
-    <Header />
     <Main />
-    <About />
-    <Footer />
+    <About {...props} />
   </>
 )
 export default Index
