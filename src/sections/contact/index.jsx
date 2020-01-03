659f7ad741f428cc884b3eb5a22a47ee15206e4d
shsangha <sssangha@ucalgary.ca>
2020-01-02 21:46:57 -0700

    used relevantpath to add contact page image from filesystem

--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -1,25 +1,43 @@
 import React from "react"
 import ContactForm from "./contactForm"
-import Footer from "../../components/footer"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
 import "./style.scss"
 
 const Contact = () => {
   return (
-    <div className="contact_page">
-      <div className="contact_page_wrapper">
-        <div className="contact_form_wrapper">
-          <div className="contact_page_form">
-            <h1 className="contact_page_header">Get In Contact</h1>
-            <ContactForm tabTrab={false} subject="contact form" />
+    <StaticQuery
+      query={graphql`
+        query {
+          image: file(relativePath: { eq: "contactimg.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 900) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      `}
+    >
+      {({
+        image: {
+          childImageSharp: { fluid },
+        },
+      }) => (
+        <div className="contact_page">
+          <div className="contact_page_wrapper">
+            <div className="contact_form_wrapper">
+              <div className="contact_page_form">
+                <h1 className="contact_page_header">Get In Contact</h1>
+                <ContactForm tabTrab={false} subject="general contact" />
+              </div>
+            </div>
+            <Img className="contact_page_img" fluid={fluid} />
           </div>
         </div>
-        <img
-          className="contact_page_img"
-          src="https://images.unsplash.com/photo-1556665490-9375cc0c29dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80"
-        />
-        <Footer />
-      </div>
-    </div>
+      )}
+    </StaticQuery>
   )
 }
 
